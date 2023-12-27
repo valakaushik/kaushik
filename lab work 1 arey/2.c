b668f9632127c3b9928f031cb11b04bbf6d47f28
valakaushik <145637185+valakaushik@users.noreply.github.com>
2023-12-27 11:03:37 +0530

    Update 2.c

--- lab work 1 arey/2.c
+++ lab work 1 arey/2.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 
-int main(){
+int main()
+{
+  int a[40], i, n, count = 0;
+  printf("enter array:");
+  scanf("%d", &n);
+  printf("\n enter elements::");
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d", &a[i]);
+  }
+  for (i = 0; i < n; i++)
+  {
+    count++;
+  }
+  printf("count is %d", count);
 
-
-
-    return 0;
+  return 0;
 }
