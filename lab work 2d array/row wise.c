29fd7d9328007163b914025140a6810f5d725a0c
valakaushik <145637185+valakaushik@users.noreply.github.com>
2023-12-29 12:30:41 +0530

    Update row wise.c

--- lab work 2d array/row wise.c
+++ lab work 2d array/row wise.c
@@ -1,27 +1,36 @@
+// row-wise
 #include <stdio.h>
-int main()
+
+int main()  
 {
-    int a[40][40], i, j, m, n, sum = 0;
-    printf("Enter array:");
-    scanf("%d", &n, &m);
-    printf("\n Enter elements::");
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            scanf("%d", &a[i][j]);
+
+    int a[30][30],i, j,m,n,sum=0;
+    printf("\enter array elements row & column");
+    scanf("%d %d",&m,&n);
+    printf("Enter metrix\n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
         }
     }
-    printf("\n print all negative elements in an array  :\n");
-    for (i = 0; i < n; i++)
-    {
-        sum = 0;
-        for (j = 0; j < n; j++) {
+    printf("Entered metrix\n");
+
+    for(i=0;i<m;i++){
+        sum=0;
+        for(j=0;j<n;j++){   
             printf("%d",a[i][j]);
-            sum = sum + a[i][j];
-        
         }
-        printf("Additon is %d\n",sum);
+            printf("\n");
     }
+
+    for(i=0;i<m;i++){
+        sum=0;
+        for(j=0;j<n;j++){   
+            sum+=a[i][j];
+        }
+            printf("sum is :%d\n",sum);
+    }
+
+
     return 0;
 }
