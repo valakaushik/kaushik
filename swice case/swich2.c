4de2ce329beb51f3182980f53bbb9dc2d84d5fbb
valakaushik <145637185+valakaushik@users.noreply.github.com>
2024-01-03 10:24:10 +0530

    Update swich2.c

--- swice case/swich2.c
+++ swice case/swich2.c
@@ -2,47 +2,52 @@
 
 int main()
 {
-     char s;
-     printf("Enter charater :");
-     scanf("%c",&s);
-     
-     switch(s){
-         case 'm': 
-         printf("\nmonday");
-         break;
-           case 't': 
-         printf("\ntuesday and thusday");
-         break;
-           case 'w': 
-         printf("\nwednesday");
-         break;
-           case 'f': 
-         printf("\nfriday");
-         break;
-           case 's': 
-         printf("\nsaturday and sunday");
-         break;
-           case 'M': 
-         printf("\nmonday");
-         break;
-        case 'T': 
-         printf("\ntuesday and thusday");
-         break;
-           case 'W': 
-         printf("\nwednesday");
-         break;
-           case 'F': 
-         printf("\nfriday");
-         break;
-           case 'S': 
-         printf("\nsaturday and sunday");
-         break;
-         
-         default:
-         printf("you enter invelid charater");
-         
-     }
-     
+   int a;
+  printf("Enter charater :");
+  scanf("%d", &a);
 
-    return 0;
+  switch (a)
+  {
+  case 1:
+    printf("\nJanuary");
+    break;
+  case 2:
+    printf("\nFebruary,");
+    break;
+  case 3:
+    printf("\nMarch");
+    break;
+  case 4:
+    printf("\n April");
+    break;
+  case 5:
+    printf("\n May,");
+    break;
+  case 6:
+    printf("\njune");
+    break;
+  case 7:
+    printf("\nJuly");
+    break;
+  case 8:
+    printf("\nAugust");
+    break;
+  case 9:
+    printf("\nSeptember");
+    break;
+  case 10:
+    printf("\n October ");
+    break;
+     case 11:
+    printf("\n November,");
+    break;
+     case 12:
+    printf("\n  December.");
+    break;
+
+  default:
+    printf("you enter invelid charater");
+  }
+
+  return 0;
 }
