408d9ec74d6025ce4396feeb1c942688a2368968
valakaushik <145637185+valakaushik@users.noreply.github.com>
2024-01-20 21:38:20 +0530

    Update 3.c

--- gathring-20240109T143247Z-001/gathring/3.c
+++ gathring-20240109T143247Z-001/gathring/3.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
-int main() {
-    char s[] = "Programming is fun";
-    int i;
-
-    for (i = 0; s[i] != '\0'; ++i);
-    
-    printf("Length of the string: %d", i);
-    return 0;
+// find Length of the String by passing String/ Character Array as an Argument using User Define Functions.
+#include<stdio.h>
+int namecount(char name[20]);
+int namecount(char name[20]){
+    int count=0,i;
+    for ( i = 0; name[i]!='\0'; i++){
+        count++;
+    }
+   return count;
+}
+void main(){
+    int a[20],n;
+    printf("enter name :");
+    scanf("%s",n);
+    printf("name count is %d",namecount(a[20]));
 }
